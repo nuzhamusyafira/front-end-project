30430db8b09d73a01b0ff838796ed6f8d3994d03
nuzhamusyafira <nuzhamusyafira@gmail.com>
2019-02-28 19:31:50 +0700

    add navbar

---
+++ screen1.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
+	<title>Screen 1</title>
+	<style type="text/css">
+		@import url(https://fonts.googleapis.com/css?family=Open+Sans);
+		body{
+			margin: 0;
+			min-width: 1488px;
+			font-family: 'Open Sans';
+		}
+		.navbar{
+			height: 80px;
+			background-color: #38C4CA;
+		}
+		.schoter{
+			font-size: 32pt;
+			color: #FFFFFF;
+			line-height: 80px;
+			padding-left: 48px;
+		}
+		.schoter .text{
+			float: left; 
+			margin: 0 38px 0 0; 
+			font-family: 'Helvetica';
+		}
+		.schoter .bar{
+			float: left; 
+			margin: 0 26px 0 0; 
+			font-size: 26px;
+		}
+		.search{
+			font-size: 20pt;
+			width: 788px;
+			color: #9B9B9B;
+			height: 50px;
+			border-color: #979797;
+			padding-left: 12px;
+			margin-top: 14px;
+			margin-right: 156px;
+		}
+		.login{
+			float: left;
+			line-height: 80px;
+		}
+		.login .text{
+			width: 68px; 
+			color: #9B9B9B; 
+			font-size: 20pt; 
+			float: left;
+		}
+		.login img{
+			width: 40px; 
+			height: 40px; 
+			border-radius: 50%; 
+			float: left; 
+			margin: 20px 26px 0 0;
+		}
+		.login .bell{
+			float: left; 
+			margin: 0 65px 0 0; 
+			font-size: 26px;
+		}
+	</style>
+</head>
+<body>
+	<div class="navbar">
+		<div class="schoter">
+			<div class="bar"><i class="fa fa-bars"></i></div>
+			<div class="text">Schoters</div>
+		</div>
+		<div style="float: left;">
+			<input class="search" type="text" placeholder="Cari beasiswa apa?">
+		</div>
+		<div class="login">
+			<div class="text">Login</div>
+			<img src="https://us.123rf.com/450wm/alekseyvanin/alekseyvanin1704/alekseyvanin170403663/76699411-user-icon-vector-profile-solid-logo-illustration-pictogram-isolated-on-white.jpg?ver=6"/>
+			<div class="bell"><i class="fa fa-bell"></i></div>
+		</div>
+	</div>
+</body>
+</html>
